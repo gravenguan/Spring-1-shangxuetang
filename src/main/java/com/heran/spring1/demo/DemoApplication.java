037ff4dbdca16c24e6757eef287f2e65a78081d8
GUANH <heran.guan@toyota.com>
2018-09-25 12:25:58 -0500

    update

--- src/main/java/com/heran/spring1/demo/DemoApplication.java
+++ src/main/java/com/heran/spring1/demo/DemoApplication.java
@@ -10,17 +10,27 @@ public class DemoApplication {
     public static void main(String[] args) {
 
         //1. Create Spring IOC
+        //ApplicationContext IOC container
+        //ClasspathXmlApplicationContext: 是ApplicationContext 接口实现类，从类路径下载config file
         ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
 
         //2. Get bean instance from IOC
+        //用id定位到IOC 容器的bean
         HelloWorld helloWorld = (HelloWorld) ctx.getBean("helloWorld");
 
         //3. call hello method
         helloWorld.hello();
 
-//        HelloWorld helloworld = new HelloWorld();
-//        helloworld.setName("Heran Guan");
-//        helloworld.hello();
+        Car car = (Car) ctx.getBean("car");
+        System.out.println(car);
+
+        car = (Car) ctx.getBean("car2");
+        System.out.println(car);
+
+
+
+        Person person = (Person) ctx.getBean("person");
+        System.out.println(person);
 
     }
 }
