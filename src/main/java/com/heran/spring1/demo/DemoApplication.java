063cd08cc1edd6635689d20384613556cece5bd0
GUANH <heran.guan@toyota.com>
2018-09-25 10:25:32 -0500

    update

--- src/main/java/com/heran/spring1/demo/DemoApplication.java
+++ src/main/java/com/heran/spring1/demo/DemoApplication.java
@@ -1,16 +1,26 @@
 package com.heran.spring1.demo;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 @SpringBootApplication
 public class DemoApplication {
 
     public static void main(String[] args) {
 
-        HelloWorld helloworld = new HelloWorld();
-        helloworld.setName("Heran Guan");
-        helloworld.hello();
+        //1. Create Spring IOC
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        //2. Get bean instance from IOC
+        HelloWorld helloWorld = (HelloWorld) ctx.getBean("helloWorld");
+
+        //3. call hello method
+        helloWorld.hello();
+
+//        HelloWorld helloworld = new HelloWorld();
+//        helloworld.setName("Heran Guan");
+//        helloworld.hello();
 
     }
 }
