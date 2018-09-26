24f640e2db4d5163ef73341e8e67df9c57ad82cd
GUANH <heran.guan@toyota.com>
2018-09-26 17:18:16 -0500

    update

---
+++ src/main/java/com/heran/spring1/demo/Car.java
@@ -0,0 +1,36 @@
+package com.heran.spring1.demo;
+
+public class Car {
+    private String brand;
+    private String corp;
+    private double price;
+    private int maxSpeed;
+
+    public Car(String brand, String corp, double price) {
+        this.brand = brand;
+        this.corp = corp;
+        this.price = price;
+    }
+
+    public Car(String brand, String corp, int maxSpeed) {
+        this.brand = brand;
+        this.corp = corp;
+        this.maxSpeed = maxSpeed;
+    }
+
+    public void setMaxSpeed(int maxSpeed) {
+        this.maxSpeed = maxSpeed;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "brand='" + brand + '\'' +
+                ", corp='" + corp + '\'' +
+                ", price=" + price +
+                ", maxSpeed=" + maxSpeed +
+                '}';
+    }
+
+
+}
---
+++ src/main/java/com/heran/spring1/demo/collect/Main.java
@@ -0,0 +1,26 @@
+package com.heran.spring1.demo.collect;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args){
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        Person person = (Person) ctx.getBean("person3");
+        System.out.println(person);
+
+        NewPerson newPerson = (NewPerson) ctx.getBean("newPerson");
+        System.out.println(newPerson);
+
+        DataSource dataSource = (DataSource) ctx.getBean("dataSource");
+        System.out.println(dataSource);
+
+        person = (Person) ctx.getBean("person4");
+        System.out.println(person);
+
+        person = (Person) ctx.getBean("person5");
+        System.out.println(person);
+
+    }
+}
